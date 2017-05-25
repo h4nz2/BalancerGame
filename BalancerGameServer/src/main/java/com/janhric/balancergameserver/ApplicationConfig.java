4570b22c8d4a9d3f6ba1f16ef3841f82f27e0bc8
Jan Hric <hric95@seznam.cz>
2017-05-25 16:01:58 +0200

    part of the previous push

--- BalancerGameServer/src/main/java/com/janhric/balancergameserver/ApplicationConfig.java
+++ BalancerGameServer/src/main/java/com/janhric/balancergameserver/ApplicationConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.balancergameserver;
+package com.janhric.balancergameserver;
 import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
--- BalancerGameServer/src/main/java/com/janhric/balancergameserver/ScoreRest.java
+++ BalancerGameServer/src/main/java/com/janhric/balancergameserver/ScoreRest.java
@@ -3,12 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.balancergameserver;
+package com.janhric.balancergameserver;
 
-/**
- *
- * @author Honza
- */
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/score")
 public class ScoreRest {
-    
+    @GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Scores getTrackInJSON() {
+		Scores score = new Scores(-1, 500, "Johny", "March 15");
+		return score;
+	}
 }
