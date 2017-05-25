c059b4d925a62336a841410da27e30f3c2cc2acc
Jan Hric <hric95@seznam.cz>
2017-05-25 17:28:11 +0200

    server returns an array of scores from db

--- BalancerGameServer/src/main/java/com/janhric/balancergameserver/ScoreRest.java
+++ BalancerGameServer/src/main/java/com/janhric/balancergameserver/ScoreRest.java
@@ -13,24 +13,28 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Path("/score")
 public class ScoreRest {
     @GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Scores getTrackInJSON() {
-            Scores score = new Scores();
+	public List<Scores> getTrackInJSON() {
+            ArrayList<Scores> scores = new ArrayList<Scores>();
             
             try {
                 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:6000/my_database", "root", "1234");            
                 Statement statement = connection.createStatement();            
                 ResultSet resultSet = statement.executeQuery("select * from scores"); 
-                if(resultSet.next()){
+                while(resultSet.next()){
+                    Scores score = new Scores();
                     score.setId(resultSet.getInt("id"));
                     score.setName(resultSet.getString("name"));
                     score.setScore(resultSet.getInt("score"));
                     score.setDate(resultSet.getString("date"));
+                    scores.add(score);
                 }
             /*while(resultSet.next()){
                 System.out.println("ID: " + resultSet.getInt("id") + ", Name: " + resultSet.getString("name") + ", Score: " + resultSet.getInt("score") + ", Date: " + resultSet.getString("date"));
@@ -40,6 +44,6 @@ public class ScoreRest {
             e.printStackTrace();
         }
             
-            return score;
+            return scores;
 	}
 }
